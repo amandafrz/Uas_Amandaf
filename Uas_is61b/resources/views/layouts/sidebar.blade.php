63ee6c50abfc7c1149192b59ef7e79d92ff116d8
amandafrz <amandafrizkaa22@gmail.com>
2024-07-28 12:48:06 +0700

    update home page

---
+++ Uas_is61b/resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,106 @@
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="{{('AdminLTE-2/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Alexander Pierce</p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+
+      <!-- /.search form -->
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+
+        <li>
+            <a href="#">
+                <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+
+            </a>
+          </li>
+
+          <li class="header">MASTER</li>
+
+          <li>
+            <a href="#">
+                <i class="fa fa-dashboard"></i> <span>Kategori</span>
+
+            </a>
+          </li>
+          <li>
+            <a href="#">
+                <i class="fa fa-dashboard"></i> <span>Produk</span>
+
+            </a>
+          </li>
+          <li>
+            <a href="#">
+                <i class="fa fa-dashboard"></i> <span>Member</span>
+
+            </a>
+          </li>
+          <li>
+            <a href="#">
+                <i class="fa fa-dashboard"></i> <span>Supplier</span>
+
+            </a>
+          </li>
+          <li class="header">Transaksi</li>
+          <li>
+            <a href="#">
+                <i class="fa fa-dashboard"></i> <span>Pengeluaran</span>
+
+            </a>
+          </li>
+          <li>
+            <a href="#">
+                <i class="fa fa-dashboard"></i> <span>Daftar Pembelian</span>
+
+            </a>
+          </li>
+          <li>
+            <a href="#">
+                <i class="fa fa-dashboard"></i> <span>Transaksi Pembelian</span>
+
+            </a>
+          </li>
+          <li>
+            <a href="#">
+                <i class="fa fa-dashboard"></i> <span>Daftar Penjualan</span>
+
+            </a>
+          </li>
+          <li>
+            <a href="#">
+                <i class="fa fa-dashboard"></i> <span>Transaksi Penjualan</span>
+
+            </a>
+          </li>
+          <li class="header">REPORT</li>
+          <li>
+            <a href="#">
+                <i class="fa fa-dashboard"></i> <span>Laporan</span>
+
+            </a>
+          </li>
+          <li class="header">SYSTEM</li>
+          li>
+            <a href="#">
+                <i class="fa fa-dashboard"></i> <span>User</span>
+
+            </a>
+          </li>
+          <li>
+            <a href="#">
+                <i class="fa fa-dashboard"></i> <span>Pengaturan</span>
+
+            </a>
+          </li>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
