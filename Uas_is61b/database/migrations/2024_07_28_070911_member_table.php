2e33eceefe5396b4a28bd24f25e01f4114013b8d
amandafrz <amandafrizkaa22@gmail.com>
2024-07-28 14:13:45 +0700

    penambahan ditabel member
    menambahkan isi dari tabel member

--- Uas_is61b/database/migrations/2024_07_28_070911_member_table.php
+++ Uas_is61b/database/migrations/2024_07_28_070911_member_table.php
@@ -11,7 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+       Schema::create('member', function (Blueprint $table) {
+           $table->increments('id_member');
+           $table->string('kode_member')->unique;
+           $table->string('nama');
+           $table->text('alamat')->nullable();
+           $table->string('telepon');
+           $table->timestamps();
+       });
     }
 
     /**
