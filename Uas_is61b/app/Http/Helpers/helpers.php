6273c2fa6f04326326145401b7ea88f6858370d5
amandafrz <amandafrizkaa22@gmail.com>
2024-08-03 22:48:35 +0700

    membuat format angka terbilang
    membuat format angka terbilang pada helpers

--- Uas_is61b/app/Http/Helpers/helpers.php
+++ Uas_is61b/app/Http/Helpers/helpers.php
@@ -3,3 +3,22 @@
 function format_uang ($angka){
     return number_format($angka, 0, ',', '.');
 }
+function
+ terbilang ($angka){
+    $angka = abs($angka);
+    $baca = array('','satu','dua','tiga','empat','lima','enam','tujuh','delapan','sembilan','sepuluh','sebelas');
+    $terbilang = '';
+
+    if($angka < 12 ){
+        $terbilang = ' ' .$baca[$angka];
+    }elseif ($angka < 20){
+        $terbilang = terbilang($angka -10) .'belas';
+    }elseif($angka < 100){
+        $terbilang = terbilang($angka /10) . 'puluh' . terbilang($angka % 10);
+    }elseif ($angka < 200){
+        $terbilang = 'seratus' . terbilang($angka -100);
+    }
+
+
+    return $terbilang;
+ }
