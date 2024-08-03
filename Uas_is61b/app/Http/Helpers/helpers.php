52f27fb0eb9fdb998286caf096ca559a4f97b6b5
amandafrz <amandafrizkaa22@gmail.com>
2024-08-03 23:16:47 +0700

    penambahan format terbilang
    penambahan format terbilang di helpers

--- Uas_is61b/app/Http/Helpers/helpers.php
+++ Uas_is61b/app/Http/Helpers/helpers.php
@@ -11,12 +11,20 @@ function
 
     if($angka < 12 ){
         $terbilang = ' ' .$baca[$angka];
-    }elseif ($angka < 20){
+    }elseif ($angka < 20){ //12 - 19
         $terbilang = terbilang($angka -10) .'belas';
-    }elseif($angka < 100){
+    }elseif($angka < 100){ //20 - 99
         $terbilang = terbilang($angka /10) . 'puluh' . terbilang($angka % 10);
-    }elseif ($angka < 200){
+    }elseif ($angka < 200){ //200 - 199
         $terbilang = 'seratus' . terbilang($angka -100);
+    }elseif ($angka < 1000){ //200 - 999
+        $terbilang = terbilang($angka /100) . ' ratus' . terbilang($angka % 100);
+    }elseif ($angka < 2000){ //100 - 1.999
+        $terbilang = 'seribu'.terbilang($angka - 1000);
+    }elseif ($angka < 1000000){ //2.000 - 999.999
+        $terbilang = terbilang($angka / 1000).'ribu'.terbilang($angka % 1000);
+    }elseif ($angka < 1000000000){ // 1.000.000 - 999.999.990
+        $terbilang = terbilang($angka / 1000000).'juta'.terbilang($angka % 1000000);
     }
 
 
