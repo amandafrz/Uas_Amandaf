2c49203861206dcfd95a069458c9606a095ef51a
amandafrz <amandafrizkaa22@gmail.com>
2024-08-04 12:57:03 +0700

    update helpers

--- Uas_is61b/app/Http/Helpers/helpers.php
+++ Uas_is61b/app/Http/Helpers/helpers.php
@@ -31,12 +31,22 @@ function
     return $terbilang;
  }
 
-function tanggal_indonesia($tgl)
+function tanggal_indonesia($tgl, $tampil_hari = true)
 {
+    $nama_hari =array(
+        'Minggu','Senin','Selasa','Rabu','Kamis','Jum\'at','Sabtu'
+    );
     $nama_bulan = array(1 =>
         'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
     );
-    // 2021-03-07
     $tahun = substr($tgl, 0, 4);
-    return $tahun;
+    $bulan = $nama_bulan[(int) substr ($tgl, 5, 2)];
+    $tanggal = substr($tgl, 8, 2);
+    $text = '';
+
+    if($tampil_hari){
+        $text .= "$tanggal $bulan $tahun";
+    }
+    $text .= "$tanggal $bulan $tahun";
+    return $text;
 }
