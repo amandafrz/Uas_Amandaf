6a9b3215e9b7c83bd524f24f720d3bff6c3543ea
amandafrz <amandafrizkaa22@gmail.com>
2024-08-06 07:46:50 +0700

    membuat tabel kategori

--- Uas_is61b/resources/views/kategori/index.blade.php
+++ Uas_is61b/resources/views/kategori/index.blade.php
@@ -16,12 +16,23 @@
         <div class="col-md-12">
           <div class="box">
             <div class="box-header with-border">
-              <h3 class="box-title">Monthly Recap Report</h3>
+              <button class="btn btn-succes xs btn-flat"><i class="fa fa-plus circle"></i>Tambah Data</button>
             </div>
-            <div class="box-body">
+            <div class="box-body table-responsive">
+                <table class="table table-stiped table-bordered">
+                    <thead>
+                        <th width="5%">No</th>
+                        <th>Kategori</th>
+                        <th width="15%"><i class="fa fa-cog"></i></th>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
             </div>
           </div>
         </div>
       </div>
 
 @endsection
+
